cb2629802bc4c4986801c8925f12bb871935042b
ThamerHussain <89868156+ThamerHussain@users.noreply.github.com>
2021-12-19 02:52:34 +0300

    Add files via upload

---
+++ stream.dart
@@ -0,0 +1,14 @@
+
+
+void main() {
+  final s1 = Stream.periodic(Duration(milliseconds: 500)
+  ,(a)=>a);
+
+  var sub = s1.listen((_)=>_);
+
+  sub.onData((data) {
+    data < 10? print(data):sub.cancel();
+  });
+  sub.onDone(()=>print('Done!'));
+  sub.onError((error)=> print(error));
+}
